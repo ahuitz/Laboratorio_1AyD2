4afa5d733e6854167515cab6039a61d7085ff9c8
EST1659109 <EST1659109@192.168.56.1>
2015-06-16 11:15:57 -0600

    Paquete para las TABLAS de la BD se crea la clase TPersona

---
+++ Laboratorio_1/src/Tablas/TPersona.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tablas;
+
+/**
+ *
+ * @author EST1659109
+ */
+public class TPersona {
+    private String nombre;
+    private String apellido;
+    private boolean conectado;
+
+    public TPersona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isConectado() {
+        return conectado;
+    }
+
+    public void setConectado(boolean conectado) {
+        this.conectado = conectado;
+    }
+    
+    @Override
+    public String toString(){
+        return "Persona"+nombre+" "+apellido;
+    }
+    
+    
+}
